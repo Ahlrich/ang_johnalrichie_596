66d3d290ae4153f53f8545900cbc57ddc5a3c3d4
John Al Richie Ang <johnrichie.18.ak@gmail.com>
2026-04-23 18:22:38 +0800

    final exam

---
+++ flutter_finals - Copy-1/flutter_finals - Copy/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\MTCL3-02\\Downloads\\flutter_finals - Copy-1\\flutter_finals - Copy" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\MTCL3-02\\Downloads\\flutter_finals - Copy-1\\flutter_finals - Copy"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\MTCL3-02\\Downloads\\flutter_finals - Copy-1\\flutter_finals - Copy\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\MTCL3-02\\Downloads\\flutter_finals - Copy-1\\flutter_finals - Copy"
+  "FLUTTER_TARGET=C:\\Users\\MTCL3-02\\Downloads\\flutter_finals - Copy-1\\flutter_finals - Copy\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NTgyYTBlN2M1NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MzQ1MmQ3MzViZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4w"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\MTCL3-02\\Downloads\\flutter_finals - Copy-1\\flutter_finals - Copy\\.dart_tool\\package_config.json"
+)
